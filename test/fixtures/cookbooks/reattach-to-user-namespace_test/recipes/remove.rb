2eb8e43d9eefe785a5e7a1a1016031556438da91
Jonathan Hartman <j@p4nt5.com>
2016-06-07 09:41:34 -0700

    Clean up RtUN before starting remove suites

--- test/fixtures/cookbooks/reattach-to-user-namespace_test/recipes/remove.rb
+++ test/fixtures/cookbooks/reattach-to-user-namespace_test/recipes/remove.rb
@@ -1,5 +1,12 @@
 # Encoding: UTF-8
 
+execute 'brew uninstall reattach-to-user-namespace' do
+  only_if 'which brew && brew list reattach-to-user-namespace'
+end
+file '/usr/local/bin/reattach-to-user-namespace' do
+  action :delete
+end
+
 include_recipe 'reattach-to-user-namespace'
 
 app_attrs = node['reattach_to_user_namespace']['app']
